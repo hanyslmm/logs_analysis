4b67ec197bc6b97750fc54102da0c8417e46bbe3
hanyslmm <hanyslmm@gmail.com>
2018-10-03 08:30:13 +0200

    update main output look

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python3
-
 import psycopg2
 import os
 import time
 from datetime import datetime
 
 localtime = time.asctime(time.localtime(time.time()))
-print("fetching log date:")
+print('\nprogram execution time stamp:\n')
 print(localtime)
+print('_______________________________\n')
 
 #connect to news database
 db = psycopg2.connect("dbname=news")
@@ -36,34 +36,34 @@ errors_day = '''
 #execute top three articles query
 cu.execute(top_articles)
 topviews = cu.fetchall()
-print ("Most Popular three articles:")
+print ("Most Popular three articles:\n")
 for i in range(0,3):
     article = topviews[i][0]
     artviews = topviews[i][1]
     print("{}- {} ___ {} views".format(i+1, article, artviews))
-    f = open('hany.txt', 'w')
-    f.write("{}- {} ___ {} views".format(i+1, article, artviews))
+print('\n_______________________________\n')
 
 #execute top three authors query
 cu.execute(top_authors)
 authors = cu.fetchall()
-print ("Most Popular three authors:")
+print ("Most Popular three authors:\n")
 for i in range(0,3):
     author = authors[i][0]
     autviews = authors[i][1]
     print("{}- {} ___ {} views".format(i+1, author, autviews))
-    #f = open('hany.txt', 'w')
-    f.write("{}- {} ___ {} views".format(i+1, author, autviews))
+print('\n_______________________________\n')
 
 #execute server daily errors
 cu.execute(errors_day)
 derrors = cu.fetchall()
-print("days more than 1% of requests lead to errors query:")
+print("Days more than 1% of requests get errors:\n")
 for derror in derrors:
+    i = 1
     day = derror[0]
     percent = round((derror[1]*100), 3)
-    print ('{}  -  {}% error'.format(day, percent))
+    print ('{}- {} ___ {}% error'.format(i, day, percent))
+    i += 1
+print('\n_______________________________\n')
 
 
-f.close()
 db.close()
