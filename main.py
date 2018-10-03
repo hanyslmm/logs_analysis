d420738f78712eb1c1179da9f8330430d9d4a477
hanyslmm <hanyslmm@gmail.com>
2018-10-03 08:10:58 +0200

    add second query top three authors

--- main.py
+++ main.py
@@ -1,31 +1,60 @@
+#! /usr/bin/env python3
+
 import psycopg2
 import os
 import time
 from datetime import datetime
 
 localtime = time.asctime(time.localtime(time.time()))
+print("fetching log date:")
 print(localtime)
 
 #connect to news database
 db = psycopg2.connect("dbname=news")
 cu = db.cursor()
 
-    #Most popular three articles
-
-query_1 = '''
+    #Most popular three articles query
+top_articles = '''
         select title, views from click_view
         limit 3;
         '''
-cu.execute(query_1)
+    #Most popular three authors query
+top_authors = '''
+        select authors.name, sum(views)
+        from authors join click_view
+        on authors.id = click_view.author
+        group by authors.name
+        order by sum(views)
+        desc;
+        '''
+    #On which days did more than 1% of requests lead to errors query
+errors_day = '''
+        select * from error_percent
+        where percentage > .01;
+        '''
+
+#execute top three articles query
+cu.execute(top_articles)
 topviews = cu.fetchall()
-#print(topviews)
 print ("Most Popular three articles:")
 for i in range(0,3):
-    #print("* {} __".format(query_1[i])
     article = topviews[i][0]
-    views = topviews[i][1]
-    print("{}- {} ___ {} views".format(i+1, article, views))
+    artviews = topviews[i][1]
+    print("{}- {} ___ {} views".format(i+1, article, artviews))
     f = open('hany.txt', 'w')
-    f.write("{}- {} ___ {} views".format(i+1, article, views))
+    f.write("{}- {} ___ {} views".format(i+1, article, artviews))
+
+#execute top three authors query
+cu.execute(top_authors)
+authors = cu.fetchall()
+print ("Most Popular three authors:")
+for i in range(0,3):
+    author = authors[i][0]
+    autviews = authors[i][1]
+    print("{}- {} ___ {} views".format(i+1, author, autviews))
+    #f = open('hany.txt', 'w')
+    f.write("{}- {} ___ {} views".format(i+1, author, autviews))
+
+
 f.close()
 db.close()
