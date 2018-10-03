a1f43fb63aacb048e39e137842fb3f3f4956cb1d
hanyslmm <hanyslmm@gmail.com>
2018-10-03 04:06:12 +0200

    modify first query popular three articles

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import psycopg2
+import os
+import time
 from datetime import datetime
+
+localtime = time.asctime(time.localtime(time.time()))
+print(localtime)
 
 #connect to news database
 db = psycopg2.connect("dbname=news")
 cu = db.cursor()
 
+    #Most popular three articles
 
-#query_1 to select the most popular three articles of all time
-select = '''select path, count(*) from log
-            group by path
-            order by count(*) DESC'''
-cu.execute(select)
-query_1 = cu.fetchall()
-db.close()
+query_1 = '''
+        select title, views from click_view
+        limit 3;
+        '''
+cu.execute(query_1)
+topviews = cu.fetchall()
+#print(topviews)
 print ("Most Popular three articles:")
-for i in range(1,4):
+for i in range(0,3):
     #print("* {} __".format(query_1[i])
-    article = query_1[i][0]
-    views = query_1[i][1]
-    print("{}- {} ___ {} views".format(i, article, views))
+    article = topviews[i][0]
+    views = topviews[i][1]
+    print("{}- {} ___ {} views".format(i+1, article, views))
+    f = open('hany.txt', 'w')
+    f.write("{}- {} ___ {} views".format(i+1, article, views))
+f.close()
+db.close()
